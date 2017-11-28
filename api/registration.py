59444a7ef42fff19c2d120b8d930fd3d5dde7ea9
maxzzze <maxwell.aliapoulios@nyu.edu>
2017-11-27 22:23:32 -0500

    14 bit integer not length

--- api/registration.py
+++ api/registration.py
@@ -22,16 +22,17 @@ class WhisperRegister:
         return r.text
 
     def verify_code(self,verfication_code, phone, password):
+        import ipdb; ipdb.set_trace()
         payload = confirmkeys.gen_sig_key()
         password = hashlib.sha256(password.encode()).hexdigest()[0:16]
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.put(api_url+'/v1/accounts/code/'+verfication_code, auth=HTTPBasicAuth(phone, password), json=payload,headers=headers,verify=False)
-        print(r,r.headers,r.status_code,r.content,r.url,r.json(),r.text)
+        req = requests.put(api_url+'/v1/accounts/code/'+verfication_code, auth=HTTPBasicAuth(phone, password), json=payload,headers=headers,verify=False)
+        print(req,req.headers,req.status_code,req.content,req.url,req.json(),req.text)
 
 
 def main():
-    number = "+17602624141"
+    number = "+12037225861"
     register = WhisperRegister()
-    #register.request_code(number)
-    register.verify_code('828-119',number,"p@ssw0rd!@#")
+    # register.request_code(number)
+    register.verify_code('282819',number,"p@ssw0rd!@#")
 main()
